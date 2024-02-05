000060caf7850d372e85d06d151a2d3a2766a423
Kirill_t <test@mail.ru>
2024-02-05 17:04:08 +0300

    Картинка Марса

--- main.py
+++ main.py
@@ -1,52 +1,29 @@
-import os
-import sys
+from flask import Flask, url_for
 
-import requests
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
-
-SCREEN_SIZE = [600, 450]
+app = Flask(__name__)
 
 
-class Example(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.getImage()
-        self.initUI()
+@app.route('/')
+def main():
+    return "Миссия Колонизация Марса"
 
-    def getImage(self):
-        map_request = "http://static-maps.yandex.ru/1.x/?ll=36.143825,51.7419908&spn=0.002,0.002&l=map"
-        response = requests.get(map_request)
 
-        if not response:
-            print("Ошибка выполнения запроса:")
-            print(map_request)
-            print("Http статус:", response.status_code, "(", response.reason, ")")
-            sys.exit(1)
+@app.route('/index')
+def index():
+    return "И на Марсе будут яблони цвести!"
 
-        # Запишем полученное изображение в файл.
-        self.map_file = "map.png"
-        with open(self.map_file, "wb") as file:
-            file.write(response.content)
 
-    def initUI(self):
-        self.setGeometry(100, 100, *SCREEN_SIZE)
-        self.setWindowTitle('Отображение карты')
+@app.route('/promotion')
+def promo():
+    return ("Человечество вырастает из детства.<br>Человечеству мала одна планета."
+            "<br>Мы сделаем обитаемыми безжизненные пока планеты.<br>И начнем с Марса!<br>Присоединяйся!")
 
-        ## Изображение
-        self.pixmap = QPixmap(self.map_file)
-        self.image = QLabel(self)
-        self.image.move(0, 0)
-        self.image.resize(600, 450)
-        self.image.setPixmap(self.pixmap)
 
-    def closeEvent(self, event):
-        """При закрытии формы подчищаем за собой"""
-        os.remove(self.map_file)
+@app.route('/image_mars')
+def image():
+    return f'''<title>Привет, Марс!</title><br><h1>Жди нас, Марс!</h1><br><img src="{url_for('static', filename='img/MARS_IMG.jpg')}" 
+           alt="здесь должна была быть картинка, но не нашлась">'''
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = Example()
-    ex.show()
-    sys.exit(app.exec())
+    app.run(port=8080, host='127.0.0.1')
